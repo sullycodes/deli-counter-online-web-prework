02081ded8af80e720dd9c78dc85e5bda38edbbfd
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-04 22:32:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+#https://learn.co/tracks/web-development-fundamentals/intro-to-ruby/arrays-and-iteration/deli-counter# Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  	if katz_deli.empty? == true
+  	puts "The line is currently empty"
+  	else
+  	puts "The line is currently:"
+    katz_deli.each_with_index {|person, index| puts "#{person} number #{index + 1}!"}
+	end
+end
+  
+def take_a_number(katz_deli, person)
+  katz_deli.push(person)
+  place= katz_deli.length
+  puts "Welcome, #{person}. You are number #{place} in ."
+end
+
